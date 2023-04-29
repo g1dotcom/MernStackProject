68cea8a5246e5ed3ad626b46946678ca8e8f0b48
Gökhan Süle <thegkhnsl@gmail.com>
2023-04-29 16:00:54 +0300

    auth işlemleri

--- server/models/user.js
+++ server/models/user.js
@@ -1,23 +1,34 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    photos: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["teacher", "student"],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ["teacher", "student"],
-    required: true,
-  },
-});
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { login, register } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get("/register");
+router.post("/register", register);
+
+router.post("/login", login);
 
 export default router;
